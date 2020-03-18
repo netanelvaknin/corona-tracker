282b884b13a8346f8e106d2fa7af45ba12b12c3c
Netanel Vaknin <emailofnetanel@gmail.com>
2020-03-19 00:51:47 +0200

    Updates

---
+++ precache-manifest.31baf0b723e1abc2022440f3d46c7566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff06ca9e9c37e0210c880c6e426c566e",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "462cbfdefacd0f08a1d5",
+    "url": "/corona-tracker/static/css/main.7d6060a6.chunk.css"
+  },
+  {
+    "revision": "52456f2f9d723b0e8d75",
+    "url": "/corona-tracker/static/js/2.91a29e32.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/corona-tracker/static/js/2.91a29e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "462cbfdefacd0f08a1d5",
+    "url": "/corona-tracker/static/js/main.7ef0f73e.chunk.js"
+  },
+  {
+    "revision": "461798bcea028e2f4893",
+    "url": "/corona-tracker/static/js/runtime-main.db302e28.js"
+  }
+]);
